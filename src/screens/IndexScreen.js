80c7d843df0a7c88c0f9bc37cb3c3e669f368b46
Victor Longon <victor.longon@etraveligroup.com>
2020-06-30 12:53:30 +0200

    ad a index screen

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const IndexScreen = () => (
+  <View>
+    <Text>Index Screen</Text>
+  </View>
+);
+
+export default IndexScreen;
